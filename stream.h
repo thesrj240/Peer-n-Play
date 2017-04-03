9af7b91326f4e5782cdbc5a6fa6b8cf86862cdcf
thesrj240 <sr.j240@gmail.com>
2017-04-03 23:17:41 +0530

    Started playing

--- stream.h
+++ stream.h
@@ -1,13 +1,16 @@
-#define SERVER_PORT 5550
-#define SERVER_NEW_CONNECT_PORT 6660
+#define SERVER_PORT 5551
+#define SERVER_NEW_CONNECT_PORT 6661
 #define SERVER_IP "172.17.15.41"
 #define MAXPEER 100
+#define BUFFER_SIZE 1024
+#define MAXPENDING 10
+#define MAXFILENAME 40
 /* TO-DO:
 1) Code Segregation
 2) data type of port
 */
 typedef struct{
-	char fileName[64];
+	char fileName[MAXFILENAME];
 	long offset;
 } streamRequest;
 
